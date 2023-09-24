6dfd62df5f3ec6e57d9866041d60e4b5e94c5db2
Li ChongXin <78329926+Followinsider@users.noreply.github.com>
2023-09-24 15:48:04 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
     // console.log('123');
     navigator.serviceWorker
-        .register('/sw_cached_site.js')
+        .register('../sw_cached_site.js')
         .then(e => console.log('注册成功', e))
         .catch(err => console.log(`报错, Error: ${err}`));
 }
